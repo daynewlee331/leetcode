c71d151eb304f8e9d7cd767a4babb41d525c9cc3
Yi Li <liyi64125837@163.com>
2016-12-10 20:50:38 -0500

    added random_pick_index.java using reservoir sampling

--- random_pick_index.java
+++ random_pick_index.java
@@ -1,5 +1,28 @@
 package leetcode;
 
-public class random_pick_index {
+import java.util.Random;
 
+public class random_pick_index {
+	int count = 0;
+    Random rand = null;
+    int[] nums = null;
+    public random_pick_index(int[] nums) {
+        this.nums = nums;
+        this.count = 0;
+        this.rand = new Random();
+    }
+    
+    public int pick(int target) {
+        int res = -1;
+        int count = 1;
+        for(int i = 0; i < nums.length; i ++){
+            if(nums[i] == target){
+                if(rand.nextInt(count) == 0){
+                    res = i;
+                }
+                count ++;
+            }
+        }
+        return res;
+    }
 }
