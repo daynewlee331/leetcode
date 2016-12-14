92df0b3e63ab41e1c4f14ebf342f6f6ba8358713
Yi Li <liyi64125837@163.com>
2016-12-14 14:58:04 -0500

    added rob house 2

--- rob_house.java
+++ rob_house.java
@@ -11,4 +11,23 @@ public class rob_house {
         }
         return dp[nums.length];
     }
+	
+	public int rob_2(int[] nums) {
+		if(nums == null || nums.length < 1) return 0;
+		if(nums.length < 2) return nums[0];
+		int[] dp1 = new int[nums.length + 1];
+		int[] dp2 = new int[nums.length + 1];
+		dp1[0] = 0;
+		dp2[0] = 0;
+		dp1[1] = nums[0];
+		dp2[1] = 0;
+		
+		for(int i = 2; i <= nums.length; i ++){
+			dp1[i] = Math.max(nums[i - 1] + dp1[i - 2], dp1[i - 1]); 
+			dp2[i] = Math.max(nums[i - 1] + dp2[i - 2], dp2[i - 1]);
+		}
+		return Math.max(dp1[nums.length - 1], dp2[nums.length]);
+	}
+	
+	
 }
