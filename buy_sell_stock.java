4d2356dd2ebc314c2d4c8b74ff064eade0d9ce06
Yi Li <liyi64125837@163.com>
2016-12-21 15:24:29 -0500

    added buy_sell_stock with cooldown

--- buy_sell_stock.java
+++ buy_sell_stock.java
@@ -1,6 +1,12 @@
 package leetcode;
 
 public class buy_sell_stock {
+	public static void main(String[] args){
+		buy_sell_stock bs = new buy_sell_stock();
+		int[] nums = {7, 1, 5, 3, 6, 4};
+		bs.maxProfit_coolDown(nums);
+	}
+	
 	//leetcode121
 	//buy and sell stock with at most one transaction
 	public int maxProfit(int[] prices) {
@@ -30,5 +36,23 @@ public class buy_sell_stock {
 			current = prices[i];
 		}
 		return res;
+    }
+	
+	//leetcode 309
+	//Best Time to Buy and Sell Stock with Cooldown
+	public int maxProfit_coolDown(int[] prices) {
+		if(prices == null || prices.length < 1) return 0;
+		int[] s0 = new int[prices.length];
+		int[] s1 = new int[prices.length];
+		int[] s2 = new int[prices.length];
+		s0[0] = 0; // At the start, you don't have any stock if you just rest
+		s1[0] = -prices[0]; // After buy, you should have -prices[0] profit. Be positive!
+		s2[0] = Integer.MIN_VALUE; // Lower base case
+		for(int i = 1; i < prices.length; i ++){
+			s0[i] = Math.max(s0[i - 1], s2[i - 1]);
+			s1[i] = Math.max(s0[i - 1] - prices[i], s1[i - 1]);
+			s2[i] = s1[i - 1] + prices[i];
+		}
+		return Math.max(s0[prices.length - 1], s2[prices.length - 1]);
     }
 }
