904c94284a30db7045af3a253f794ceb3777e48b
Yi Li <liyi64125837@163.com>
2016-12-21 16:06:36 -0500

    added leetcode 199

--- next_right_in_tree.java
+++ next_right_in_tree.java
@@ -1,5 +1,10 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class next_right_in_tree {
 	class TreeLinkNode {
 		int val;
@@ -7,6 +12,15 @@ public class next_right_in_tree {
 		TreeLinkNode(int x) { val = x; }
 	}
 	
+	class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+	
+	//leetcode 116
+	//Populating Next Right Pointers in Each Node
 	//hint:
 	//always begin from the most left node
 	//connecting children nodes (nodes of lower level) 
@@ -24,4 +38,25 @@ public class next_right_in_tree {
         	pre = pre.left;
         }
     }
+	
+	//leetcode 199
+	//Binary Tree Right Side View
+	public List<Integer> rightSideView(TreeNode root) {
+		List<Integer> res = new ArrayList<Integer>();
+		if(root == null) return res;
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.offer(root);
+		while(!queue.isEmpty()){
+			int size = queue.size();
+			TreeNode tmp = null;
+			for(int i = 0; i < size; i ++){
+				tmp = queue.poll();
+				if(tmp.left != null) queue.offer(tmp.left);
+				if(tmp.right != null) queue.offer(tmp.right);
+			}
+			res.add(tmp.val);
+		}
+		return res;
+    }
+	
 }
