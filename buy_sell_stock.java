5438e0b84327e12c52ad1caf6cbaffbbf0145492
Yi Li <liyi64125837@163.com>
2016-12-21 12:54:32 -0500

    added leetcode 122: buy and sell stock II

--- buy_sell_stock.java
+++ buy_sell_stock.java
@@ -16,4 +16,19 @@ public class buy_sell_stock {
         }
         return res;
     }
+	
+	//leetcode 122
+	//Best Time to Buy and Sell Stock II
+	public int maxProfit2(int[] prices) {
+		if(prices == null || prices.length < 1) return 0;
+		int current = prices[0];
+		int res = 0;
+		for(int i = 1; i < prices.length; i ++){
+			if(prices[i] > current){
+				res += prices[i] - current;
+			}
+			current = prices[i];
+		}
+		return res;
+    }
 }
