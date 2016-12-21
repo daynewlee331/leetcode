a21111551fa876a85bdd828b44492f63d4e2ee3d
Yi Li <liyi64125837@163.com>
2016-12-20 20:23:07 -0500

    added combination_sum4 leetcode 377

--- combination_sum.java
+++ combination_sum.java
@@ -2,10 +2,17 @@ package leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
 public class combination_sum {
+	public static void main(String[] args){
+		combination_sum cs = new combination_sum();
+		int[] nums = {1,2,3};
+		cs.combinationSum4(nums, 5);
+	}
+	
 	public List<List<Integer>> combinationSum(int[] candidates, int target) {
 		List<List<Integer>> res = new ArrayList<List<Integer>>();
 		if(candidates == null || candidates.length < 1) return res;
@@ -68,6 +75,46 @@ public class combination_sum {
 			for(int i = start; i < nums.length; i ++){
 				cur.add(nums[i]);
 				combinationSum3_helper(i + 1, nums, res, cur, target - nums[i], k);
+				cur.remove(cur.size() - 1);
+			}
+		}
+	}
+	
+	//leetcode 377
+	//combination sum IV
+	public int combinationSum4(int[] nums, int target) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		if(nums == null || nums.length < 1 || target < 0) return 0;
+		return combinationSum4_helper(nums, target, map);
+	}
+	
+	public int combinationSum4_helper(int[] nums, int target, HashMap<Integer, Integer> map) {
+		int res = 0;
+		if(target < 0) return 0;
+		if(target == 0) return 1;
+		if(map.containsKey(target)) return map.get(target);
+		for(int i = 0; i < nums.length; i ++){
+			res += combinationSum4_helper(nums, target - nums[i], map);
+		}
+		map.put(target, res);
+		return res;
+	}
+	
+	public int myCombinationSum4(int[] nums, int target) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		if(nums == null || nums.length < 1) return 0;
+		myCombinationSum4(nums, target, res, new ArrayList<Integer>());
+		return res.size();
+    }
+	
+	public void myCombinationSum4(int[] nums, int target, List<List<Integer>> res, List<Integer> cur){
+		if(target == 0){
+			res.add(new ArrayList<>(cur));
+			return;
+		}else if(target > 0){
+			for(int i = 0; i < nums.length; i ++){
+				cur.add(nums[i]);
+				myCombinationSum4(nums, target - nums[i], res, cur);
 				cur.remove(cur.size() - 1);
 			}
 		}
