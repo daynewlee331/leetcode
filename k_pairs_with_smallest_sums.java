a942adefc1bedf4becaae15f9a3faf697a90e8de
Yi Li <liyi64125837@163.com>
2017-01-06 17:38:16 +0800

    added leetcode 373. Find K Pairs with Smallest Sums

---
+++ k_pairs_with_smallest_sums.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class k_pairs_with_smallest_sums {
+	class Pair{
+		int[] pair;
+		int index; //current index of nums2
+		long sum;
+		Pair(int index, int n1, int n2){
+			this.index = index;
+			pair = new int[]{n1, n2};
+			sum = (long)n1 + (long)n2;
+		}
+		
+	}
+	//leetcode 373. Find K Pairs with Smallest Sums
+	 public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+		 List<int[]> res = new ArrayList<int[]>();
+		 if(nums1 == null || nums2 == null || nums1.length < 1 || nums2.length < 1) return res;
+		 int length2 = nums2.length;
+		 PriorityQueue<Pair> pq = new PriorityQueue<Pair>(k, new Comparator<Pair>(){
+			@Override
+			public int compare(Pair o1, Pair o2) {
+				// TODO Auto-generated method stub
+				return Long.compare(o1.sum, o2.sum);
+			}});
+		 
+		 for(int i = 0; i < nums1.length && i < k; i ++){
+			 pq.offer(new Pair(0, nums1[i], nums2[0]));
+		 }
+		 
+		 for(int j = 1; j <= k && !pq.isEmpty(); j ++){
+			 Pair p = pq.poll();
+			 res.add(p.pair);
+			 if(p.index < length2 - 1){//make sure we are still in range
+				 int next = p.index + 1;
+				 pq.offer(new Pair(next, p.pair[0], nums2[next]));
+			 }
+		 }
+		 return res;
+	 }
+}
