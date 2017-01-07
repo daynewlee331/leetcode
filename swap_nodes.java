52a1dfe2829cf64a0688f4fe41950af6883454b2
Yi Li <liyi64125837@163.com>
2017-01-07 22:05:49 +0800

    added leetcode 25

--- swap_nodes.java
+++ swap_nodes.java
@@ -1,10 +1,18 @@
 package leetcode;
 
 public class swap_nodes {
-	class ListNode {
-		int val;
-		ListNode next;
-		ListNode(int x) { val = x; }
+	
+	public static void main(String[] args){
+		swap_nodes sn = new swap_nodes();
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+		head.next.next.next.next.next = new ListNode(6);
+		head.next.next.next.next.next.next = new ListNode(7);
+		head.next.next.next.next.next.next.next = new ListNode(8);
+		sn.reverseKGroup(head, 3);
 	}
 	
 	public ListNode swapPairs(ListNode head) {
@@ -21,4 +29,26 @@ public class swap_nodes {
 		node.val = buff;
 		swapHelper(node.next.next);
 	}
+	
+	//leetcode 25. Reverse Nodes in k-Group
+	public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        int count = 0;
+        while(curr != null && count != k){//find k+1 th node
+        	curr = curr.next;
+        	count ++;
+        }
+        if(count == k){
+        	curr = reverseKGroup(curr, k);
+        	while(count > 0){
+        		ListNode tmp = head.next;
+        		head.next = curr;
+        		curr = head;
+        		head = tmp;
+        		count --;
+        	}
+        	head = curr;
+        }
+        return head;
+    }
 }
