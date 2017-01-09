5103d852215687c36360b55bffa330e13d96b9af
Yi Li <liyi64125837@163.com>
2017-01-09 21:20:03 +0800

    added 202 happy number

---
+++ happy_and_ugly_number.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.HashSet;
+
+public class happy_and_ugly_number {
+	//leetcode 202. Happy Number
+	public boolean isHappy(int n) {
+		HashSet<Integer> set = new HashSet<Integer>();
+        while(set.add(n)){
+        	int sum = 0;
+        	while(n > 0){
+        		int tmp = n % 10;
+        		sum += tmp * tmp;
+        		n = n / 10;
+        	}
+        	if(sum == 1) return true;
+        	else n = sum;
+        }
+        return false;
+    }
+	
+	//leetcode 263. Ugly Number
+	 public boolean isUgly(int num) {
+		 
+	 }
+}
