e6905505e6750973e5b246f6305338a90dba4cfc
Yi Li <liyi64125837@163.com>
2017-01-09 12:29:22 +0800

    added leetcode 291 Word Pattern II

--- word_pattern.java
+++ word_pattern.java
@@ -1,9 +1,41 @@
 package leetcode;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class word_pattern {
+	public static void main(String[] args){
+		word_pattern wp = new word_pattern();
+		wp.wordPatternMatch("abba", "redbluebluered");
+	}
+	
 	//leetcode 291. Word Pattern II
+	public boolean wordPatternMatch(String pattern, String str){
+		HashMap<String, String> map = new HashMap<String, String>();
+		HashSet<String> set = new HashSet<String>();
+		return wordPatternMatchHelper(pattern, str, map, set);
+	}
+	
+	public boolean wordPatternMatchHelper(String pattern, String str, HashMap<String, String> map, HashSet<String> set){
+		if(pattern.isEmpty()) return str.isEmpty();
+		if(map.containsKey(pattern.substring(0,1))){
+			String word = map.get(pattern.substring(0, 1));
+			if(str.length() < word.length() || !str.substring(0, word.length()).equals(word)) return false;
+			if(wordPatternMatchHelper(pattern.substring(1), str.substring(word.length()), map, set)) return true;
+		}else{
+			for(int i = 1; i <= str.length(); i ++){
+				String sub = str.substring(0, i);
+				if(set.contains(sub)) continue;
+				String key = pattern.substring(0, 1);
+				map.put(key, sub);
+				set.add(sub);
+				if(wordPatternMatchHelper(pattern.substring(1), str.substring(i), map, set)) return true;
+				set.remove(sub);
+				map.remove(key);
+			}
+		}
+		return false;
+	}
 	
 	//leetcode 205 Isomorphic Strings
 	public boolean isIsomorphic(String s, String t) {
