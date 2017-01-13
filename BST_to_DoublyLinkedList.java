3755e749e7e9b6aab776b540c8d4f6118ea5746f
Yi Li <liyi64125837@163.com>
2017-01-14 00:13:27 +0800

    added two files

--- BST_to_DoublyLinkedList.java
+++ BST_to_DoublyLinkedList.java
@@ -26,5 +26,4 @@ public class BST_to_DoublyLinkedList {
 		lastNode = root;
 		helper(root.right);
 	}
-	
 }
--- clone_graph.java
+++ clone_graph.java
@@ -5,22 +5,26 @@ import java.util.HashMap;
 
 public class clone_graph {
 	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-		if(node == null) return null;
-		HashMap<Integer, UndirectedGraphNode> map = new HashMap<>();//hashMap to store known nodes
+		if (node == null)
+			return null;
+		HashMap<Integer, UndirectedGraphNode> map = new HashMap<>();// hashMap
+																	// to store
+																	// known
+																	// nodes
 		UndirectedGraphNode res = new UndirectedGraphNode(node.label);
 		map.put(node.label, res);
-		cloneGraphHelper(res, node,map);
+		cloneGraphHelper(res, node, map);
 		return res;
 	}
-	
-	public void cloneGraphHelper(UndirectedGraphNode copy, UndirectedGraphNode origin
-			, HashMap<Integer, UndirectedGraphNode> map) {
+
+	public void cloneGraphHelper(UndirectedGraphNode copy, UndirectedGraphNode origin,
+			HashMap<Integer, UndirectedGraphNode> map) {
 		copy.neighbors = new ArrayList<UndirectedGraphNode>();
-		for(int i = 0; i < origin.neighbors.size(); i ++ ){
+		for (int i = 0; i < origin.neighbors.size(); i++) {
 			UndirectedGraphNode neighbor = origin.neighbors.get(i);
-			if(map.containsKey(neighbor.label)){
+			if (map.containsKey(neighbor.label)) {
 				copy.neighbors.add(map.get(neighbor.label));
-			}else{
+			} else {
 				UndirectedGraphNode tmp = new UndirectedGraphNode(neighbor.label);
 				map.put(neighbor.label, tmp);
 				copy.neighbors.add(tmp);
