07d43e970966e3f188ed693c8e3324d74e01ef95
Yi Li <liyi64125837@163.com>
2017-01-16 16:14:08 +0800

    added LIS binary search

--- Longest_Increasing_Subsequence.java
+++ Longest_Increasing_Subsequence.java
@@ -1,9 +1,37 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Longest_Increasing_Subsequence {
 	public static void main(String[] args){
 		Longest_Increasing_Subsequence obj = new Longest_Increasing_Subsequence();
 		obj.lengthOfLIS(new int[]{2, 5, 3, 7});
+	}
+	
+	public int lengthOfLISOpt(int[] nums) {
+		if(nums.length <= 1) return nums.length;
+		List<Integer> buff = new ArrayList<Integer>();
+		buff.add(nums[0]);
+		for(int num : nums){
+			int len = buff.size();
+			int left = 0; int right = len - 1;
+			while(left <= right){
+				int mid = (right - left) / 2 + left;
+				if(buff.get(mid) < num){
+					left = mid + 1;
+				}else{
+					right = mid - 1;
+				}
+			}
+			if(buff.size() > left){
+				buff.set(left, num);
+			}else{
+				buff.add(num);
+			}
+		}
+		
+		return buff.size();
 	}
 	
 	public int lengthOfLIS(int[] nums) {
@@ -13,7 +41,7 @@ public class Longest_Increasing_Subsequence {
         
         for(int i = 1; i < nums.length; i ++){
         	for(int j = 0; j < i; j ++){
-        		if(nums[i] > nums[j]) if(dp[j] + 1 > dp[i]) dp[i] = dp[j] + 1;
+        		if((nums[i] > nums[j]) && (dp[j] + 1 > dp[i])) dp[i] = dp[j] + 1;
         	}
         }
         
