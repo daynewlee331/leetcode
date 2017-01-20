933677cd93d7c6998b48a299dcd44fcb12fdbebe
Yi Li <liyi64125837@163.com>
2017-01-21 01:56:24 +0800

    added 114. Flatten Binary Tree to Linked List

--- BST_to_DoublyLinkedList.java
+++ BST_to_DoublyLinkedList.java
@@ -1,21 +1,29 @@
 package leetcode;
 
 public class BST_to_DoublyLinkedList {
-	class TreeNode {
-		int val;
-		TreeNode left;
-		TreeNode right;
-		TreeNode(int x) { val = x; }
-	}
 	
 	private TreeNode lastNode;
+	
+	public static void main(String[] args){
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.left.left = new TreeNode(3);
+		root.left.right = new TreeNode(4);
+		root.right = new TreeNode(5);
+		BST_to_DoublyLinkedList sol = new BST_to_DoublyLinkedList();
+		TreeNode head = sol.BST2DoublyLinkedlist(root);
+		while ( head != null ){
+            System.out.print(head.val + "->");
+            head = head.right;
+        }
+	}
 	
 	public TreeNode BST2DoublyLinkedlist(TreeNode root){
 		if(root == null) return null;
 		TreeNode fakeHead = new TreeNode(-1);
 		this.lastNode = fakeHead;
 		helper(root);
-		return this.lastNode.right;
+		return fakeHead.right;
 	}
 	
 	public void helper(TreeNode root){
---
+++ Flatten_BT_to_LinkedList.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.ArrayList;
+
+public class Flatten_BT_to_LinkedList {
+	//leetcode 114
+	public static void main(String[] args){
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+		Flatten_BT_to_LinkedList sol = new Flatten_BT_to_LinkedList();
+		sol.flatten(root);
+	}
+	
+	public void flatten(TreeNode root) {
+		ArrayList<TreeNode> prev = new ArrayList<TreeNode>();
+		prev.add(new TreeNode(-1));
+		helper(root, prev);
+    }
+	
+	public void helper(TreeNode root, ArrayList<TreeNode> prev){
+		if(root == null) return;
+		TreeNode right = root.right;
+		if(prev != null){
+			prev.get(0).right = root;
+			prev.get(0).left = null;
+		}
+		prev.set(0, root);
+		helper(root.left, prev);
+		helper(right, prev);
+	}
+}
