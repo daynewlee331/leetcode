c119e5c64ad9c2b5d73f7f3fbb563fc6c151837e
Yi Li <liyi64125837@163.com>
2017-01-24 11:16:49 -0500

    added leetcode 360 public int[] sortTransformedArray(int[] nums, int a, int b, int c) {

--- reverse_print_linkedlist.java
+++ reverse_print_linkedlist.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Stack;
+
 public class reverse_print_linkedlist {
 	public static void main(String[] args){
 		reverse_print_linkedlist sol = new reverse_print_linkedlist();
@@ -7,12 +9,24 @@ public class reverse_print_linkedlist {
 		head.next = new ListNode(2);
 		head.next.next = new ListNode(3);
 		head.next.next.next = new ListNode(4);
-		sol.printElement(head);
+		sol.reversePrint(head);
 	}
 	
 	public void printElement(ListNode node){
 		if(node == null) return;
 		printElement(node.next);
 		System.out.print(node.val + " ");
+	}
+	
+	public void reversePrint(ListNode node){
+		if(node == null) return;
+		Stack<ListNode> stack = new Stack<ListNode>();
+		while(node != null){
+			stack.add(node);
+			node = node.next;
+		}
+		while(!stack.isEmpty()){
+			System.out.print(stack.pop().val + " ");
+		}
 	}
 }
---
+++ sort_transformed_arr.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+public class sort_transformed_arr {
+	//leetcode 360
+	public static void main(String[] args){
+		sort_transformed_arr sol = new sort_transformed_arr();
+		int[] test = sol.sortTransformedArray(new int[]{-4, -2, 2, 4}, 1,3,5);
+		for(int num: test) System.out.print(num + " ");
+		System.out.println();
+		int[] test2 = sol.sortTransformedArray(new int[]{-4, -2, 2, 4}, -1,3,5);
+		for(int num: test2) System.out.print(num + " ");
+	}
+	
+	public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
+		//1.a>0, two ends in original array are bigger than center if you learned middle school math before.
+		//2.a<0, center is bigger than two ends.
+		if(nums == null || nums.length < 1) return new int[0];
+		if(nums.length == 1) return new int[]{nums[0]};
+		int n = nums.length; int i = 0; int j =  n - 1;
+		int[] res = new int[n];
+		int index = n - 1;
+		if(a < 0) index = 0;
+		while(i <= j){
+			if(a < 0){
+				res[index ++] = transform(nums[i], a, b ,c) < transform(nums[j], a, b, c)? transform(nums[i ++], a, b ,c): transform(nums[j --], a, b, c);
+			}else{
+				res[index--] = transform(nums[i], a, b ,c) > transform(nums[j], a, b, c)? transform(nums[i ++], a, b ,c) : transform(nums[j --], a, b, c);
+			}
+		}
+		return res;
+	}
+	
+	public int transform(int x, int a, int b, int c){
+		return a * x * x + b * x + c;
+	}
+}
