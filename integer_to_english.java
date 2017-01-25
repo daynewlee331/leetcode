11f1bf007a6554f7b3a2d16d9d14bf1c1bd29189
Yi Li <liyi64125837@163.com>
2017-01-25 15:58:49 -0500

    updated leetcode 273

--- integer_to_english.java
+++ integer_to_english.java
@@ -7,48 +7,46 @@ import java.util.Stack;
 public class integer_to_english {
 	public static void main(String[] args){
 		integer_to_english ie = new integer_to_english();
-		System.out.println(ie.two_digits(0));
 	}
 	private String[] lessThan20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-	private String[] tens = {"*","*","Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+	private String[] tens = {"","","Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 	private String[] thousands = {"","","Thousand", "Million", "Billion"};
 	private String[] hundreds = {"","One Hundred","Two Hundred","Three Hundred","Four Hundred","Five Hundred","Six Hundred","Seven Hundred","Eight Hundred","Nine Hundred"};	
 	
-	public String numberToWords(int num) {
+    public String numberToWords(int num) {
 		if(num == 0) return "Zero";
-        
-        Stack<Integer> stack = new Stack<Integer>();
-        while(num > 0){
-        	int tmp = num % 1000;
-        	num = num / 1000;
-        	stack.add(tmp);
-        }
-        StringBuilder res = new StringBuilder();
-        while(!stack.isEmpty()){
-        	int size = stack.size();
-        	String digits = three_digits(stack.pop());
-        	res.append(digits);
-        	if(size - 1 != 0 && digits!= "") res.append(" ");
-        	if(digits != "") res.append(thousands[size]);
-        	if(size - 1 != 0 && digits!= "") res.append(" ");
-        }
-        return res.toString().trim();
-    }
-	
-	public String three_digits(int num){
-		if(num > 99){
-			String digits = two_digits(num % 100);
-			return digits.equals("")? hundreds[num / 100]: hundreds[num / 100] + " " + digits;
+		Stack<String> stack = new Stack<String>();
+		while(num != 0){
+			String digits = threeDigits(num % 1000);
+			stack.push(digits);
+			num /= 1000;
 		}
-		return two_digits(num);
+		StringBuilder res = new StringBuilder();
+		while(!stack.isEmpty()){
+			int k = stack.size();
+			String digits = stack.pop();
+			if(!digits.equals("")){
+				res.append(digits);
+				res.append(" ");
+				res.append(thousands[k]);
+				res.append(" ");
+			}
+		}
+		
+		return res.toString().trim();
 	}
 	
-	public String two_digits(int num){
-		if(num < 20){
-			return this.lessThan20[num];
-		}else{
-			String digit = lessThan20[num % 10];
-    		return digit.equals("")? tens[num / 10]: tens[num / 10] + " " + digit;
-		}
+	public String threeDigits(int num){
+		if(num <= 99) return twoDigits(num);
+		int tmp1 = num % 100;
+		String res = hundreds[num / 100];
+		return tmp1 == 0? res : res + " " + twoDigits(tmp1); 
+	}
+	
+	public String twoDigits(int num){
+		if(num < 20) return lessThan20[num];
+		int tmp1 = num / 10;
+		int tmp2 = num % 10;
+		return tmp2 ==0 ?tens[tmp1] : tens[tmp1] + " " + lessThan20[tmp2];
 	}
 }
