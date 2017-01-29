d865fcc791bea3b2b911583cdb047638a8562215
Yi Li <liyi64125837@163.com>
2017-01-29 17:37:32 -0500

    added LC63 unique path 2

--- unqie_path.java
+++ unqie_path.java
@@ -1,6 +1,11 @@
 package leetcode;
 
 public class unqie_path {
+	public static void main(String[] args){
+		unqie_path sol = new unqie_path();
+		sol.uniquePathsWithObstacles(new int[][]{{0}});
+	}
+	
 	public int uniquePaths(int m, int n) {
 		if(m == 0 || n == 0) return 0;
 		if(m == 1 || n == 1) return 1;
@@ -18,7 +23,25 @@ public class unqie_path {
 				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
 			}
 		}
-		
 		return dp[m - 1][n - 1];
+    }
+	
+	public int uniquePathsWithObstacles(int[][] a) {
+        int m = a.length;
+        int n = a[0].length;
+        
+        for(int i = 0; i < m; i ++){
+        	for(int j = 0; j < n; j ++){
+        		if(a[i][j] == 1) a[i][j] = 0;
+        		else{
+        			if(i == 0 && j == 0) a[i][j] = 1;
+            		else if(i == 0 && j > 0) a[i][j] = a[i][j - 1];
+            		else if(j == 0 && i > 0) a[i][j] = a[i - 1][j];
+            		else a[i][j] = a[i - 1][j] + a[i][j - 1];
+        		}
+        	}
+        }
+        
+        return a[m - 1][n - 1];
     }
 }
