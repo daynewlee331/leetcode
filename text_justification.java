39319716396c657bd2d83c0670bc14abcf30e5da
Yi Li <liyi64125837@163.com>
2017-02-06 09:54:16 -0500

    added shorted solution for LC68

--- text_justification.java
+++ text_justification.java
@@ -4,81 +4,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class text_justification {
-	public static void main(String[] args){
-		text_justification tj = new text_justification();
-		//String[] test = {"This", "is", "an", "example", "of", "text", "justification."};
-		String[] test = {"What","must","be","shall","be."};
-		List<String> res = tj.fullJustify(test, 12);
-		System.out.println();
-	}
 	
+	//leetcode 68
 	public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> res = new ArrayList<String>();
-        if(words == null || words.length == 0) return res;
-        int curL = 0;
-        int count = 0;
-        StringBuilder sb = new StringBuilder();
-        ArrayList<String> buffer = new ArrayList<String>();
-        for(int i = 0; i < words.length; i ++){
-        	if(curL + words[i].length() + count <= maxWidth){
-        		buffer.add(words[i]);
-        		curL += words[i].length();
-        		count ++;
-			} else {
-				if(count == 1){//only a single word
-					int tmp = maxWidth - curL;
-					sb.append(buffer.get(0));
-					for(int j = 0; j < tmp; j ++){
-						sb.append(" ");
-					}
-				}else{
-					int tmp = (maxWidth - curL) / (count - 1);
-					int m = (maxWidth - curL) % (count - 1);
-					for(int j = 0; j < buffer.size(); j ++){
-						int nspace = (j + 1) <= m ? tmp+1 : tmp;
-						if(j != buffer.size() - 1){
-							sb.append(buffer.get(j));
-							for(int k = 0; k < nspace; k ++){
-								sb.append(" ");
-							}
-						}else{
-							sb.append(buffer.get(j));
-						}
-					}
-				}
-				
-				res.add(sb.toString());
-				// clear everything
-				curL = 0;
-				buffer = new ArrayList<String>();
-				sb = new StringBuilder();
+		List<String> res = new ArrayList<String>();
+		if(words == null || words.length < 1) return res;
+		int charNum = 0;
+		ArrayList<String> buffer = new ArrayList<String>();
+		StringBuilder builder = null;
+		for(int i = 0; i < words.length; i ++){
+			if(charNum + buffer.size() + words[i].length() <= maxWidth){
+				charNum += words[i].length();
 				buffer.add(words[i]);
-				curL = words[i].length();
-				count = 1;
-        	}
-        }
-        if (buffer.size() == 1) {
-			int tmp = maxWidth - curL;
-			sb.append(buffer.get(0));
-			for (int j = 0; j < tmp; j++) {
-				sb.append(" ");
-			}
-			res.add(sb.toString());
-		} else if (buffer.size() > 1) {
-			for (int j = 0; j < buffer.size(); j++) {
-				if (j != buffer.size() - 1) {
-					sb.append(buffer.get(j));
-					sb.append(" ");
-				} else {
-					sb.append(buffer.get(j));
+			}else{
+				builder = new StringBuilder();
+				if(buffer.size() == 1){
+					int diff = maxWidth - charNum;
+					builder.append(buffer.get(0));
+					builder.append(generateSpaces(diff));
+				}else{
+					int avg = (maxWidth - charNum) / (buffer.size() - 1);
+					int extra = (maxWidth - charNum) % (buffer.size() - 1);
+					for(int j = 0; j < buffer.size(); j ++){
+						int nspace = (j + 1) <= extra? avg + 1: avg;
+						builder.append(buffer.get(j));
+						if(j != buffer.size() - 1) builder.append(generateSpaces(nspace));
+					}
 				}
+				res.add(builder.toString());
+				buffer.clear();
+				buffer.add(words[i]);
+				charNum = words[i].length();
 			}
-			int tmp = maxWidth - sb.length();
-			for (int k = 0; k < tmp; k++) {
-				sb.append(" ");
-			}
-			res.add(sb.toString());
 		}
-        return res;
+		
+		builder = new StringBuilder();
+		if(buffer.size() == 1){
+			int diff = maxWidth - charNum;
+			builder.append(buffer.get(0));
+			builder.append(generateSpaces(diff));
+			res.add(builder.toString());
+		}else{
+			int num = 0;
+			for(int j = 0; j < buffer.size(); j ++){
+				builder.append(buffer.get(j));
+				num += buffer.get(j).length();
+				builder.append(" ");
+				num ++;
+			}
+			builder.append(generateSpaces(maxWidth - num));
+			res.add(builder.toString());
+		}
+		return res;
     }
+	
+	public String generateSpaces(int num){
+		StringBuilder builder = new StringBuilder();
+		for(int i = 0; i < num; i ++){
+			builder.append(" ");
+		}
+		return builder.toString();
+	}
 }
